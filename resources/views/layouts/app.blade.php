dd32f05a8843d6140f17ed000c921fadb0151c31
Vitor Moreira de Oliveira <vitormoroli@gmail.com>
2023-08-30 18:54:20 -0300

    mudando a cor de fundo do menu hamburguer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
                 <a class="navbar-brand text-white" href="{{ url('/') }}">
                     E-commerce
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
