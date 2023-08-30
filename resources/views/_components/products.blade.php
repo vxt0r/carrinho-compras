07d03ff908d8a99f6405bab3cb0796fc3cc0bf22
Vitor Moreira de Oliveira <vitormoroli@gmail.com>
2023-08-30 18:54:56 -0300

    adicionando responsividade na página

--- resources/views/_components/products.blade.php
+++ resources/views/_components/products.blade.php
@@ -1,4 +1,4 @@
-<div class="row bg-dark w-50 my-4 mx-auto">
+<div class="col-sm-8 row bg-dark my-4 mx-auto rounded">
     @foreach ($products as $product)
         <div class="col-md-6 d-flex flex-column align-items-center fs-5 mb-5">
             <img src="/images/{{$product->imagem}}" class="mt-3">
@@ -7,12 +7,14 @@
             @if (Route::currentRouteName() == 'admin.index')
                 <a href="{{route('admin.edit',$product->id)}}" class="bg-primary text-white fs-5 p-1 rounded text-decoration-none">Editar</a>
             @else
-                <form action="{{route('home.add')}}" method="POST" class="w-25 d-flex flex-column">
+            <div class="col-sm-3 col-md-5 col-lg-3">
+                <form action="{{route('home.add')}}" method="POST" class="d-flex flex-column">
                     @csrf
                     <input type="hidden" name="id" value="{{$product->id}}">
-                    <input type="number" name="qtd" min="1" max="15" value="1">
-                    <button type="submit" class="bg-success text-white mt-2 rounded border border-0">Adicionar</button>
+                    <input type="number" name="qtd" min="1" max="15" value="1" class="mb-2">
+                    <button type="submit" class="btn btn-success text-white rounded border border-0">Adicionar</button>
                 </form>
+            </div>
             @endif
         </div>
         @endforeach
