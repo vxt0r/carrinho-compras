de654b2fe99f83ff0e77dcf4a18d400bfd7b3176
Vitor Moreira de Oliveira <vitormoroli@gmail.com>
2023-08-31 19:09:20 -0300

    mudando a cor do card do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body bg-dark text-white">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
